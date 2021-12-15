22bd9ac6b1bc9f00bad5349c31b021e0c8f7beda
Nicola Gigante <88109014+NiGiga@users.noreply.github.com>
2021-12-15 13:44:54 +0000

    funzione

--- lezione2.py
+++ lezione2.py
@@ -1,7 +1,21 @@
-def hello(name):
-    print(10 + name)
+# Python program to find sum of all
+# elements in list using recursion
+ 
+# creating a list
+list1 = [11, 5, 17, 18, 23]
+ 
+# creating sum_list function
+def sumOfList(list, size):
+   if (size == 0):
+     return 0
+   else:
+     return list[size - 1] + sumOfList(list, size - 1)
+  
+# Driver code    
+total = sumOfList(list1, len(list1))
+ 
+print("Sum of all elements in given list: ", total)
 
-hello(1)
 
     
     
