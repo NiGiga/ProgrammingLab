3eb57a9e6cb36a40312584fba19761d3d9531ec7
Nicola Gigante <88109014+NiGiga@users.noreply.github.com>
2021-12-22 16:19:19 +0000

    Es lezione 4

--- lezione4.py
+++ lezione4.py
@@ -4,5 +4,32 @@
 #3) abbia un metodo “get_data()” che torni i dati dal file CSV come lista  di liste, ad es: [ ['01-01-2012', '266.0'], ['01-02-2012', '145.9'], ... ] Provatelo sul file “shampoo_sales.csv”.
 
 #creo classe
-class CSVFile(self, name):
-    def __init__()
+class CSVFile():
+    def __init__(self, name):
+        self.name = name
+    #creo la funzione
+    def get_data(self):
+        my_file = open(self.name, 'r')
+        #creo una lista vuoto
+        values = []
+        #splitto sulla virgola
+        for line in my_file:
+            elemnts = line.split(',')
+             #if(!='Data')
+            values.append(elemnts)
+        #chiudo il file
+        my_file.close()
+        #ritorno i valori
+        return (values)
+#attribuisco a shampoo CSVFile
+shampoo = CSVFile('shampoo_sales.csv')
+#attribuisco a my_data la funzione
+my_data = shampoo.get_data()
+#stampo a schermo i valori
+print(my_data)
+
+
+
+
+
+    
