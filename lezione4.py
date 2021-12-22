71f1cf6d3c2e93d4dff3017e2112d062f45f27c4
Nicola Gigante <88109014+NiGiga@users.noreply.github.com>
2021-12-22 15:32:28 +0000

    Lezione 4 slides

---
+++ lezione4.py
@@ -0,0 +1,8 @@
+#Create un oggetto CSVFile che rappresenti un file CSV, e che:
+#1) venga inizializzato sul nome del file csv, e
+#2) abbia un attributo “name” che ne contenga il nome
+#3) abbia un metodo “get_data()” che torni i dati dal file CSV come lista  di liste, ad es: [ ['01-01-2012', '266.0'], ['01-02-2012', '145.9'], ... ] Provatelo sul file “shampoo_sales.csv”.
+
+#creo classe
+class CSVFile(self, name):
+    def __init__()
--- test.py
+++ test.py
@@ -1,14 +1,68 @@
-# Python program to find sum of all
-# elements in list using recursion
+#importo il random
+import random
+ #creo l'oggetto
+class Person():
+    #inizializzo
+    def __init__(self,name,surname):
+        #setto name e surname
+        self.name    = name
+        self.surname = surname
+    #oggetto persona
+    def __str__(self):
+        return 'Persona {} {}'.format(self.name, self.surname)
+    #creo funzione randomica
+    def say_hi(self):
+        #imposto random rang
+        random_num = random.randint(0,2)
+        #creo le variabili
+        if random_num == 0:
+            print('Hello, I am {} {}'.format(self.name, self.surname))
+        elif random_num == 1:
+            print('Hei there, I am {} {}'.format(self.name, self.surname))
+        elif random_num == 2:
+            print('Yoh Broh, {} there'.format(self.name))
  
-# creating a list
-list1 = [11, 5, 17, 18, 23]
- 
-# creating sum_list function
-def sumOfList(list):
-      return sum(list)
-  
-# Driver code    
-total = sumOfList(list1)
- 
-print("Sum of all elements in given list: ", total)
+#estendo l'oggetto
+class Student(Person):
+    #sovvrascrivo l'oggetto persona per dare un titolo (studente)
+    def __str__(self):
+        return 'studente {} {}'.format(self.name, self.surname)
+     #richiamo la funzione originale
+    def original_say_hi(self):
+        super().say_hi()
+#estendo l'oggetto
+class Professor(Person):
+    #sovvrascrivo l'oggetto persona per dare un titolo(Prof.)
+    def __str__(self):
+        return 'Prof. {} {}'.format(self.name, self.surname)
+    #sovvrascrivo la funzione per dare un sauto più consono al professore
+    def say_hi(self):
+        print('Hello, I am Professor {} {}'.format(self.name, self.surname))
+    #richiamo l'originale
+    def original_say_hi(self):
+        super().say_hi()
+
+#stampo a schermo person
+print('________________________________________')
+
+person = Person('Nicola','Giga')
+print(person)
+person.say_hi()
+
+#stampo a schermo Professor
+print('________________________________________')
+
+prof = Professor('Don','Matteo')
+print(prof)
+prof.say_hi()
+prof.original_say_hi()
+
+#stampo a schermo student
+print('________________________________________')
+
+studente = Student('Giovanni','Muciaccia')
+print(studente)
+studente.original_say_hi()
+
+print('________________________________________')
+
